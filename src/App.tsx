9037b08222109a347356d2cf2a660c35198223c0
Vitor Sousa <112447987+vitorsousa77@users.noreply.github.com>
2025-07-13 13:49:00 -0300

    Primeiro commit do projeto

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    console.log('Ok, the app is running!');
+    return (
+        <div>
+            <h1>Welcome to the Pomodoro App</h1>
+        </div>
+    )
+}
+
+export { App };
